c9d2644124beb16306c394d0db49c1c13818fc83
Dmitry Sky <dmitry.sky@icloud.com>
2016-10-29 10:51:54 -0600

    use child_addes event to add li elements

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+// get elements
 var preObject = document.getElementById("object")
-var dbRef  = firebase.database().ref().child("object")
-dbRef.on("value", snap => {
+var ulList      = document.getElementById("list")
+
+// create references
+var dbRefObject = firebase.database().ref().child("object")
+var dbRefList   = dbRefObject.child("hobbies")
+
+// sync object changes
+dbRefObject.on("value", snap => {
   preObject.innerText = JSON.stringify(snap.val(), null, 1);
+})
+
+// sync list changes
+dbRefList.on("child_added", snap => {
+  const li = document.createElement("li")
+  li.innerText = snap.val()
+  ulList.appendChild(li)
 })
