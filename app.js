ca424c2b2a1fb6c9cc003cd2e271bbffdf3e7f88
Dmitry Sky <dmitry.sky@icloud.com>
2016-10-31 09:00:20 -0600

    display dynamic status bar, upload file to firebase

--- app.js
+++ app.js
@@ -76,3 +76,33 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
     btnSignUp.classList.remove("hide")
   }
 })
+
+// get elements
+var uploader   = document.getElementById("uploader")
+var fileButton = document.getElementById("fileButton")
+
+// listen for file selection
+fileButton.addEventListener("change", function(e) {
+  // get file
+  var file = e.target.files[0]
+
+  // create a storage ref
+  var storageRef = firebase.storage().ref("sweet_gifs/" + file.name)
+
+  // upload file
+  var task = storageRef.put(file)
+
+  // update progress bar
+  task.on("state_changed",
+    function progress(snapshot) {
+      var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+      uploader.value = percentage
+    },
+    function error(err) {
+
+    },
+    function complete() {
+
+    }
+  )
+})
